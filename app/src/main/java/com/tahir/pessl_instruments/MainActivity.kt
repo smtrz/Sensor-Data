62218dba86d23852f39cd67f67e81e3acc0cbec5
Tahir Raza <smtrz@yahoo.com>
2020-02-04 10:36:46 +0500

    - Created basic cardview and skeleton of the app

---
+++ app/src/main/java/com/tahir/pessl_instruments/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tahir.pessl_instruments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
